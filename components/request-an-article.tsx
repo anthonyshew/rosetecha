c002401d5200b85e05d3979f0b92fd5c3dc26128
Anthony Shew <anthony@inthezone.dev>
2022-07-11 18:38:15 -0700

    Clean up and do two terms.'

---
+++ components/request-an-article.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const RequestAnArticle = () => (
+  <p>
+    You can send a message to `anthony.shew@vercel.com` or submit a Github issue
+    on the repository for this project.
+  </p>
+)
--- theme.config.js
+++ theme.config.js
@@ -4,9 +4,11 @@ export default {
   titleSuffix: ' – Nextra',
   logo: (
     <>
-      <span className="mr-2 font-extrabold hidden md:inline">Nextra</span>
+      <span className="mr-2 font-extrabold hidden md:inline">
+        The Rosetecha Stone
+      </span>
       <span className="text-gray-600 font-normal hidden md:inline">
-        The Next.js Static Site Generator
+        Tech for Non-Technicals
       </span>
     </>
   ),
