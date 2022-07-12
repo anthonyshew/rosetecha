e4d451d1203eef097b6cfda4740db3ec6fb0c6ba
Anthony Shew <anthony@inthezone.dev>
2022-07-12 11:59:52 -0700

    All initial key terms done

--- theme.config.js
+++ theme.config.js
@@ -1,7 +1,6 @@
 export default {
-  github: 'https://github.com/shuding/nextra',
-  docsRepositoryBase: 'https://github.com/shuding/nextra/blob/master',
-  titleSuffix: ' – Nextra',
+  github: 'https://github.com/anthony-shew/vercel-dictionary',
+  titleSuffix: ' – The Rosetecha Stone',
   logo: (
     <>
       <span className="mr-2 font-extrabold hidden md:inline">
@@ -18,13 +17,13 @@ export default {
       <meta name="theme-color" content="#ffffff" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <meta httpEquiv="Content-Language" content="en" />
-      <meta name="description" content="Nextra: the Next.js site builder" />
-      <meta name="og:description" content="Nextra: the Next.js site builder" />
+      <meta name="description" content="The Rosetecha Stone: Tech for Non-Technicals" />
+      <meta name="og:description" content="The Rosetecha Stone: Tech for Non-Technicals" />
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:image" content="https://nextra.vercel.app/og.png" />
       <meta name="twitter:site:domain" content="nextra.vercel.app" />
       <meta name="twitter:url" content="https://nextra.vercel.app" />
-      <meta name="og:title" content="Nextra: Next.js static site generator" />
+      <meta name="og:title" content="The Rosetecha Stone: Tech for Non-Technicals" />
       <meta name="og:image" content="https://nextra.vercel.app/og.png" />
       <meta name="apple-mobile-web-app-title" content="Nextra" />
       <link
